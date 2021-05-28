36bbc709e20f797f10e518bed356f1eae21205a6
kharismarizqii <rizqikharisma@gmail.com>
2021-05-28 22:47:42 +0700

    Add ReverseInteger Solution

---
+++ ReverseInteger.kt
@@ -0,0 +1,36 @@
+class ReverseInteger {
+    fun reverse(x: Int): Int{
+        if(x==0) return 0
+        val number = x.toString().reversed()
+        var newString = ""
+        var invalid = true
+        for (i in number){
+            if (invalid){
+                if (i == '0'){
+                    continue
+                } else {
+                    invalid = false
+                    newString = newString + i
+                }
+            } else {
+                if(i!='-'){
+                    newString = newString + i
+                }
+            }
+        }
+        if(isInIntRange(newString.toLong())){
+            return if (isNegative(x)) "-$newString".toInt() else newString.toInt()
+        } else {
+            return 0
+        }
+
+    }
+    private fun isNegative(x: Int): Boolean{
+        return if(x < 0) true else false
+    }
+    private fun isInIntRange(value: Long) : Boolean {
+        return if(value>2147483647 || value<-2147483648) false else true
+    }
+}
+
+
