549aa4dc68e22585f303fdd46252cb407b8b5aa1
kharismarizqii <rizqikharisma@gmail.com>
2021-06-10 10:35:12 +0700

    add PowerOfThree solution

---
+++ PowerOfThree.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+class PowerOfThree {
+    fun isPowerOfThree(n: Int): Boolean {
+        var number = n
+        if (n <= 0) return false
+        while (number > 1){
+            if (number%3 != 0){
+                return false
+            } else {
+                number=number/3
+            }
+        }
+        return true
+    }
+}
