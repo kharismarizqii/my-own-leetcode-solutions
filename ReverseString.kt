fc063ff868e061a52c68afee465db20bfc9a5992
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-09 14:24:17 +0700

    optimize ReverseString solution

--- ReverseString.kt
+++ ReverseString.kt
@@ -2,24 +2,9 @@ package leetcode
 
 class ReverseString {
     fun reverseString(s: CharArray): Unit {
-        recursive(s,0,s.size-1)
-
-    }
-    private fun recursive(s: CharArray, start: Int, size: Int) {
-        var i = start
-        var j = size
-        // i from first, j from last
-        if(i == j || i>j){
-            return
-        }
-        else{
-            val temp = s[i]
-            s[i] = s[j]
-            s[j] = temp
-            i++
-            j--
-            recursive(s,i,j)
-            return
+        var newS = s.reversed()
+        for(i in 0 until s.size){
+            s[i] = newS[i]
         }
     }
 }
