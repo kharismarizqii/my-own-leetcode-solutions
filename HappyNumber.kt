e9df25bf65ff7da77e7e96c3b303447efa59bdd8
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-17 11:06:15 +0700

    add HappyNumber solution

---
+++ HappyNumber.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+class HappyNumber {
+    fun isHappy(n: Int): Boolean {
+        var slow = n
+        var fast = n
+        do {
+            slow = digitSquareSum(slow)
+            fast = digitSquareSum(fast)
+            fast = digitSquareSum(fast)
+        }while(slow!=fast)
+        if(slow == 1) return true
+        else return false
+    }
+
+    fun digitSquareSum(num: Int): Int{
+        var n = num
+        var sum = 0
+        var tmp = 0
+        while(n!=0){
+            tmp = n%10
+            sum += tmp*tmp
+            n/=10
+        }
+        return sum
+    }
+}
