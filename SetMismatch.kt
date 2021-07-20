92f82aa625bd7b8d71316bba1eb20c73847a2e14
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-20 11:59:35 +0700

    optimize SetMismatch solution

--- SetMismatch.kt
+++ SetMismatch.kt
@@ -4,23 +4,16 @@ import java.util.*
 
 class SetMismatch {
     fun findErrorNums(nums: IntArray): IntArray {
-        var result : IntArray = intArrayOf(0,0)
-        val filterNums = nums.distinct()
-        var missingValue = 0
-        for(i in 1..nums.size){
-            if(!filterNums.contains(i)){
-                missingValue = i
+        var duplicate = 0
+        val list = ArrayList<Int>()
+        for((i,value) in nums.withIndex()){
+            if(!list.contains(value)){
+                list.add(value)
+            }else{
+                duplicate = value
             }
         }
-        val stack = Stack<Int>()
-        for(i in 0 until nums.size){
-            if(!stack.contains(nums[i]))
-                stack.push(nums[i])
-            else{
-                result = intArrayOf(nums[i], missingValue)
-            }
-
-        }
-        return result
+        val difference = (1..nums.size).toCollection(ArrayList<Int>()).minus(list)
+        return intArrayOf(duplicate, difference[0])
     }
 }
