adc844b8f7f7181accee9fad391bb4bf02e835cf
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-26 13:22:49 +0700

    optimize PowerOfThree solution

--- PowerOfThree.kt
+++ PowerOfThree.kt
@@ -4,12 +4,9 @@ class PowerOfThree {
     fun isPowerOfThree(n: Int): Boolean {
         var number = n
         if (n <= 0) return false
-        while (number > 1){
-            if (number%3 != 0){
-                return false
-            } else {
-                number=number/3
-            }
+        while (number > 1){ 
+            if (number % 3 != 0) return false
+            else number = number / 3
         }
         return true
     }
