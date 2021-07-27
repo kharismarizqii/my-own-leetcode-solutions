6254a3758dbb36f78791ea1b58733fc06355fc5a
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-27 19:04:43 +0700

    optimize TwoSum solution

--- TwoSum.kt
+++ TwoSum.kt
@@ -3,8 +3,9 @@ package leetcode
 class TwoSum {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         var result : IntArray = intArrayOf(0,0)
-        for(i in 0 until nums.size){
-            val current = nums[i]
+        var current : Int
+        for(i in nums.indices){
+            current = nums[i]
             for(j in nums.size-1 downTo 0){
                 if (current+nums[j]==target){
                     result = intArrayOf(i,j)
