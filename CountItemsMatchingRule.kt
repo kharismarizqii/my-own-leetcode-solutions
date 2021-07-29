67fcdcecc15f19626cb7bea1c0a568cfcf1913db
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-29 08:45:22 +0700

    optimize CountItemsMatchingRule solution

--- CountItemsMatchingRule.kt
+++ CountItemsMatchingRule.kt
@@ -3,18 +3,13 @@ package leetcode
 class CountItemsMatchingRule {
     fun countMatches(items: List<List<String>>, ruleKey: String, ruleValue: String): Int {
         var result = 0
+        val index = when(ruleKey){
+            "type" -> 0
+            "color" -> 1
+            else -> 2
+        }
         for(i in items){
-            when(ruleKey){
-                "type" -> {
-                    if(i[0]==ruleValue) result++
-                }
-                "color" -> {
-                    if(i[1]==ruleValue) result++
-                }
-                "name" -> {
-                    if(i[2]==ruleValue) result++
-                }
-            }
+            if(i[index]==ruleValue) result++
         }
         return result
     }
