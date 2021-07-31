978064c104a02e0bc36d541cf8e25ec93217ec21
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-07-31 19:53:33 +0700

    optimize DetectCapital solution

--- DetectCapital.kt
+++ DetectCapital.kt
@@ -6,29 +6,18 @@ class DetectCapital {
         var state = true
         var rest = false
         var result = true
-        var valid = true
-        for(i in 0 until word.length){
-            if(i==0){
-                first = isCapital(word[i])
-            } else {
-                if(i==1) {
-                    rest = isCapital(word[i])
-                }
-                if(i>1 && isCapital(word[i])!= state){
-                    result = false
-                    break
-                }
-                state = isCapital(word[i])
+        for(i in word.indices){
+            if(i==0) first = isCapital(word[i])
+            else if(i==1) rest = isCapital(word[i])
+            else if(i>1 && isCapital(word[i])!= state){
+                result = false
+                break
             }
+            state = isCapital(word[i])    
         }
-        if(first==true && rest==false) valid = true
-        if(first==true && rest==true) valid = true
-        if(first==false && rest==true) valid = false
-
-        return result && valid
+        
+        return result && !(!first && rest)
     }
 
-    private fun isCapital(c: Char): Boolean{
-        return c.toByte()>=65 && c.toByte()<=90
-    }
+    private fun isCapital(c: Char) = c.toByte()>=65 && c.toByte()<=90
 }
