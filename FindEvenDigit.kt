d51e5d01da54b194cdce09bc133b23d0cf4f240b
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-08-01 16:43:45 +0700

    optimize FindEvenDigit Solution

--- FindEvenDigit.kt
+++ FindEvenDigit.kt
@@ -1,13 +1,21 @@
 package leetcode
 
+import kotlin.math.abs
+import kotlin.math.log10
+
 class FindEvenDigit {
     fun findNumbers(nums: IntArray): Int {
         var count = 0
-        for(i in nums){
-            if(i.toString().length %2 == 0){
+        nums.iterator().forEach {
+            if(it.length() %2 == 0){
                 count++
             }
         }
         return count
+    }
+
+    fun Int.length() = when(this) {
+        0 -> 1
+        else -> log10(abs(toDouble())).toInt() + 1
     }
 }
