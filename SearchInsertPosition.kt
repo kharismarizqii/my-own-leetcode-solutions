5996df5372e7d97d4022e04c9777ed8392d9f1b8
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-08-03 15:50:10 +0700

    add SearchInsertPosition solution

--- SearchInsertPosition.kt
+++ SearchInsertPosition.kt
@@ -3,11 +3,8 @@ package leetcode
 class SearchInsertPosition {
     fun searchInsert(nums: IntArray, target: Int): Int {
         var result = nums.size
-        for(i in 0 until nums.size){
-            if(nums[i]==target){
-                result = i
-                break
-            } else if(nums[i]>target){
+        for(i in nums.indices){
+            if(nums[i]>=target){
                 result = i
                 break
             }
