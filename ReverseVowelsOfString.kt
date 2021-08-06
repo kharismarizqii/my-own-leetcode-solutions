1315c82b19381b5ea21d4a1e8c7822bf62159ca2
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-08-06 21:10:14 +0700

    optimize ReverseViwelsOfString solution

--- ReverseVowelsOfString.kt
+++ ReverseVowelsOfString.kt
@@ -5,18 +5,16 @@ import java.util.*
 class ReverseVowelsOfString {
     fun reverseVowels(s: String): String {
         val vowels = Stack<Char>()
-        for(i in s){
-            if(i == 'a' || i == 'i' || i == 'u' || i == 'e' || i == 'o' || i == 'A' || i == 'I' || i == 'U' || i == 'E' || i == 'O'){
-                vowels.push(i)
-            }
-        }
-
         val sb = StringBuilder(s)
 
+        for(i in s){
+            if(i == 'a' || i == 'i' || i == 'u' || i == 'e' || i == 'o' || i == 'A' || i == 'I' || i == 'U' || i == 'E' || i == 'O')
+                vowels.push(i)
+        }
+
         for((index, i) in s.withIndex()){
-            if(i == 'a' || i == 'i' || i == 'u' || i == 'e' || i == 'o' || i == 'A' || i == 'I' || i == 'U' || i == 'E' || i == 'O'){
+            if(i == 'a' || i == 'i' || i == 'u' || i == 'e' || i == 'o' || i == 'A' || i == 'I' || i == 'U' || i == 'E' || i == 'O')
                 sb.setCharAt(index,vowels.pop())
-            }
         }
         return sb.toString()
     }
