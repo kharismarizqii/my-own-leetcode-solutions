59148fed1d5f4b73a6934c3cf26323f1cf73bbb9
Kharisma Rizqi <rizqikharisma@gmail.com>
2021-08-21 22:04:08 +0700

    Reduce power of three code

--- PowerOfThree.kt
+++ PowerOfThree.kt
@@ -6,7 +6,7 @@ class PowerOfThree {
         if (n <= 0) return false
         while (number > 1){ 
             if (number % 3 != 0) return false
-            else number = number / 3
+            else number /= 3
         }
         return true
     }
